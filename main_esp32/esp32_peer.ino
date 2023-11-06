86845c0ad5299c6b92b370fde8b91ff67b6abe52
stbs <steveehs2000@gmail.com>
2023-11-05 21:20:23 -0300

    Código para 2 agentes

--- main_esp32/esp32_peer.ino
+++ main_esp32/esp32_peer.ino
@@ -1,33 +1,47 @@
-void blink(){
-  digitalWrite(INTERNAL_LED, HIGH);
-  delay(100);
-  digitalWrite(INTERNAL_LED, LOW);
-  delay(100);
-  digitalWrite(INTERNAL_LED, HIGH);
-  delay(100);
-  digitalWrite(INTERNAL_LED, LOW);
-}
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+  if (PeerActual == 1) {
 
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status){
-  //Serial.print("\r\nLast Packet Send Status:\t");
+      if (status = ESP_NOW_SEND_SUCCESS) {
+        flag_resend = 0;
+      } else {
+        Serial.println("Delivery Fail");
+        flag_resend = 1; //No entregado, se debe reenviar
+      }
+  } 
 
-  if (status = ESP_NOW_SEND_SUCCESS) {
-    //Serial.println("Deliver Success");
-    flag_resend = 0;
-  
-  } else {
-    //Serial.println ("Delivery Fail");
-    flag_resend = 1;
-  
+  else if (PeerActual == 2) 
+  {
+      if (status = ESP_NOW_SEND_SUCCESS) {
+        Serial.println("Delivery Success");
+      } else {
+        Serial.println("Delivery Fail");
+      }
   }
 }
 
-void OnDataRecv(const esp_now_recv_info_t *esp_now_info, const uint8_t *incomingData, int len){
-  t_1 = micros();
-  RTT = t_1 - t_0;
-  memcpy(&esp_buffer_rx, incomingData, sizeof(esp_buffer_rx));
-  Serial.printf("%d Bytes Received\n", len); 
-  Serial.printf ("RTT = %Au [us], Estimated Delay = %u [us]\n", RTT, RTT/2);
-  Serial.printf("%u\n", RTT/2);
-  flag_recv = 1;
+
+void OnDataRecv(const esp_now_recv_info_t *esp_now_info, const uint8_t *incomingData, int len)
+{
+  if (PeerActual == 1) {
+    t_1 = esp_timer_get_time();;
+    RTT = t_1 - t_0;
+
+    memcpy(&esp_buffer_rx, incomingData, sizeof(esp_buffer_rx));
+
+    Serial.print("$RTT:");
+    Serial.print(RTT);
+    Serial.println("}");
+
+    flag_recv = true; //Recibido, se puede reenviar
+    delay(2000);
+  } 
+
+  else if (PeerActual == 2) 
+  {
+    memcpy(&esp_buffer_tx, incomingData, sizeof(esp_buffer_tx));
+    esp_now_send(mac_addr_E1, (uint8_t*)&esp_buffer_tx, sizeof(esp_buffer_tx));
+  }
+
 }
+
--- main_esp32/main_esp32.ino
+++ main_esp32/main_esp32.ino
@@ -1,10 +1,26 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-#define INTERNAL_LED 2
+//#define INTERNAL_LED 2
 
-//Registrar Peer Actual
-esp_now_peer_info_t peerInfo;
+//////////////////////////////////////////////////
+//Cambiar en función del Peer que sube el código//
+//////////////////////////////////////////////////
+uint8_t PeerActual = 1; // Peer E1
+// uint8_t PeerActual = 2; // Peer E2
+//////////////////////////////////////////////////
+
+//uint8_t mac_broadcast[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff}
+
+//E1  : B8:D6:1A:AB:4D:8C
+//E2  : B8:D6:1A:AC:03:60
+
+uint8_t mac_addr_E1[] = {0xB8, 0xD6, 0x1A, 0xAB, 0x4D, 0x8C};
+uint8_t mac_addr_E2[] = {0xB8, 0xD6, 0x1A, 0xAC, 0x03, 0x60};
+
+esp_now_peer_info_t peerInfo_E1;
+esp_now_peer_info_t peerInfo_E2;
+
 
 typedef struct struct_message {
     int numero;
@@ -15,25 +31,24 @@ typedef struct struct_message {
 struct_message esp_buffer_tx;
 struct_message esp_buffer_rx;
 
-//uint8_t mac_broadcast[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff}
 
-uint8_t mac_addr_E1[] = {0xB8, 0xD6, 0x1A, 0xAB, 0x4D, 0x8C};
-uint8_t mac_addr_E2[] = {0xB8, 0xD6, 0x1A, 0xAC, 0x03, 0x60};
+bool flag_recv = 0;
+bool flag_resend = 1;
 
-bool flag_recv = 1;
-bool flag_resend = 0;
+//uint32_t t_0, t_1, RTT;
+int64_t t_0, t_1, RTT;
 
-uint32_t t_0, t_1, RTT;
-
-void setup(){
+void setup()
+{
   Serial.begin(115200);
 
-  pinMode(INTERNAL_LED, OUTPUT);
-  digitalWrite(INTERNAL_LED, LOW);
+  // pinMode(INTERNAL_LED, OUTPUT);
+  // digitalWrite(INTERNAL_LED, LOW);
 
   WiFi.mode(WIFI_STA); //Modo WiFi Station
 
-  if (esp_now_init() != ESP_OK){
+  if (esp_now_init() != ESP_OK)
+  {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
@@ -42,25 +57,48 @@ void setup(){
   esp_now_register_send_cb(OnDataSent);
   esp_now_register_recv_cb(OnDataRecv);
 
+  // Configurar la estructura peerInfo_E1 con la información de E1
+  memcpy(peerInfo_E1.peer_addr, mac_addr_E1, sizeof(mac_addr_E1));
+  peerInfo_E1.channel = 1;
+  peerInfo_E1.encrypt = false; // Configurar cifrado según tus necesidades
 
-  memcpy(peerInfo.peer_addr, mac_addr_E1, 6);
-  
-  peerInfo.channel = 1;
-  peerInfo.encrypt = false;
+  // Configurar la estructura peerInfo_E2 con la información de E2
+  memcpy(peerInfo_E2.peer_addr, mac_addr_E2, sizeof(mac_addr_E2));
+  peerInfo_E2.channel = 1;
+  peerInfo_E2.encrypt = false;
 
-  if(esp_now_add_peer(&peerInfo) != ESP_OK){
-    Serial.println("Failed to add peer");
+  if(esp_now_add_peer(&peerInfo_E1) != ESP_OK)
+  {
+    Serial.println("Failed to add peer E1");
     return;
   }
-}
-
-
-void loop(){
-  blink();
-  if( flag_recv || flag_resend ){ 
-    t_0 = micros(); //idem 
-    esp_now_send(NULL, (uint8_t*)&esp_buffer_tx, sizeof(esp_buffer_tx));
-    flag_recv = 0;
+  
+  if(esp_now_add_peer(&peerInfo_E2) != ESP_OK)
+  {
+    Serial.println("Failed to add peer E2");
+    return;
   }
-  delay(1000)
+
+  Serial.print("PEER ACTUAL -> E");
+  Serial.println(PeerActual);
 }
+
+void loop()
+{
+  if (PeerActual == 1) 
+  {  
+      if( flag_recv || flag_resend )
+      { 
+        t_0 = esp_timer_get_time(); //idem 
+        esp_now_send(mac_addr_E2, (uint8_t*)&esp_buffer_tx, sizeof(esp_buffer_tx));
+        
+        flag_recv = 0;
+      } 
+  } 
+
+  else if (PeerActual == 2) 
+  {
+    delay(50);
+  }
+}
+
